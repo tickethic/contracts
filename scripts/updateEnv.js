5a617cbf30041f155a7dca6502c475c24bbdce0c
Dario Vujica <vujica.dario@gmail.com>
2025-11-25 00:03:05 +0100

    fix env deployment

--- scripts/updateEnv.js
+++ scripts/updateEnv.js
@@ -1,17 +1,17 @@
-const fetch = require("node-fetch");
-const fs = require("fs");
+import fetch from "node-fetch";
+import { readFileSync } from "fs";
 
 const githubToken = process.env.GITHUB_PAT_TICKETHIC;
 const repoOwner = "tickethic";
 const repoName = "tickethic";
-const envName = "dev"; // ou "production"
+const envName = "dev";
 
-// Charge les adresses du fichier JSON
-const addresses = JSON.parse(fs.readFileSync("dist/contract-addresses.json"));
+// Charge les adresses
+const addresses = JSON.parse(readFileSync("dist/contract-addresses.json", "utf-8"));
 
 const variablesToUpdate = {
   NEXT_PUBLIC_EVENT_MANAGER_CONTRACT: addresses.eventManager,
-  NEXT_PUBLIC_EVENT_CONTRACT: addresses.event, // si pertinent
+  NEXT_PUBLIC_EVENT_CONTRACT: addresses.event,
   NEXT_PUBLIC_TICKET_CONTRACT: addresses.ticket,
   NEXT_PUBLIC_ORGANIZATOR_CONTRACT: addresses.organizator,
   NEXT_PUBLIC_ARTIST_CONTRACT: addresses.artist,
@@ -20,7 +20,7 @@ const variablesToUpdate = {
 async function updateVariables() {
   for (const [key, value] of Object.entries(variablesToUpdate)) {
     const url = `https://api.github.com/repos/${repoOwner}/${repoName}/environments/${envName}/variables/${key}`;
-    await fetch(url, {
+    const response = await fetch(url, {
       method: "PATCH",
       headers: {
         Authorization: `Bearer ${githubToken}`,
@@ -29,8 +29,13 @@ async function updateVariables() {
       },
       body: JSON.stringify({ value }),
     });
-    console.log(`Updated ${key} to ${value}`);
+
+    if (!response.ok) {
+      console.error(`Error updating ${key}:`, await response.text());
+    } else {
+      console.log(`Updated ${key} to ${value}`);
+    }
   }
 }
 
-updateVariables();
+updateVariables().catch(console.error);
